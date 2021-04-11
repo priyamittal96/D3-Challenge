049b90dc418a73bb38cfe05ab2879ecd26863924
shantipriya30 <75239249+shantipriya30@users.noreply.github.com>
2021-04-10 17:55:02 -0700

    xText

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -16,10 +16,38 @@ var labelArea = 110;
 var tPadBot = 40; 
 var tPadLeft = 40; 
 
-// svg
+// Generate svg
 var svg = d3
     .select("#scatter")
     .append("svg")
     .attr("width",width)
     .attr("height",height)
     .attr("class", "chart")
+
+// Dot radius
+var cicrRadius ;
+function crGet(){
+    if (width <= 530){
+        circRadius = 5;
+    } else {
+        circRadius = 10;
+    }
+}
+
+crGet();
+
+// group svg together
+svg.append("g").attr("class","xText");
+
+// xText will allow us to select group without excess code
+var xText = d3.select("xText");
+function xTextRefresh(){
+    xText.attr(
+        "transform",
+        "translate(" +
+        ((width - labelArea) / 2 + labelArea) + 
+        ", " + 
+        (height - margin - tPadBot) + 
+        ")"
+    )
+}
